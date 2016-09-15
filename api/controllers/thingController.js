f0165b390ebdc6331196bd9d2a5a980b2f12ee93
Joseph Brown <joe@sendlove.io>
2016-09-15 15:11:56 -0700

    got mongoose working

--- api/controllers/thingController.js
+++ api/controllers/thingController.js
@@ -1,28 +1,62 @@
 // thingController
 'use strict';
 var mongoose = require('mongoose');
-var Things = require('../../models/things');
 var util = require('util');
+// tbd
+var bodyParser = require('body-parser');
+var Verify = require('verify');
 
-// see if we need these
-// var bodyParser = require('body-parser');
-// var Verify = require('./verify');
+var Things = require('../../models/things');
 
+
+// Exports all the functions to perform on the db
 module.exports = {
-  getThings: getThings
+  getThingsArray, addThing
 };
 
-function getThings(req, res) {
+function getThingsArray(req, res) {
   var latitude = req.swagger.params.latitude.value || 34.0522
   var longitude = req.swagger.params.longitude.value || 118.2437;
   
-  //testing
   var things = []; 
-  var obj = [{id: 1, name: "hello, world!", description: "first workout", ownerId: 1, latitude: latitude, longitude: longitude },{id: 2, name: "hello, world, again!", description: "second workout", ownerId: 1, latitude: latitude, longitude: longitude }];
+  
+  //testing
+  var obj = [{id: "4567467467", name: "hello, world!", description: "first workout", ownerId: "345636", latitude: latitude, longitude: longitude },{id: "45646786", name: "hello, world, again!", description: "second workout", ownerId: "3546365", latitude: latitude, longitude: longitude }];
   for(var i in obj){
     things.push(obj[i])
   }
   
-  // this sends back a JSON response which is a single string
-  res.json(things).pretty;
+  res.json(things);
+}
+/*
+{
+  "name": "hello, world!",
+  "description": "first workout",
+  "ownerId": 1,
+  "latitude": 1,
+  "longitude": 1
+}
+*/
+function addThing (req, res) {
+  //testing
+  var latitude = req.body.latitude;
+  var longitude = req.body.longitude;
+  var name = req.body.name;
+  var description = req.body.description;
+  var ownerId = req.body.ownerId;
+  
+  Things.create(req.body, function(err, obj) {
+      if (err) throw err;
+      var id = obj._id;
+      console.log("created thing id: " + id);
+      res.json(obj);
+  });
+  
+  // testing
+  // var id = ""; // set this from the returned object
+  // var obj = {id: id, name: name, description: description, ownerId: ownerId, latitude: latitude, longitude: longitude };
+  // res.json(obj);
+  
+  
+   
 }
--- models/things.js
+++ models/things.js
@@ -1,2 +1,40 @@
 // things.js
 
+// fields: name, description, ownerId, latitude, longitude // tbd: image, category
+// schemas: parts (which have messages), tags
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+Schema = mongoose.Schema;
+
+
+
+var thingSchema=new Schema( {
+    name: {
+        type: String, required: true
+    }
+    , description: {
+        type: String, required: true
+    }
+    , ownerId: {
+        type: Number, required: true
+    }
+    , latitude: {
+        type: Number, required: true
+    }
+    , longitude: {
+        type: Number, required: true
+    }
+}
+
+, {
+    timestamps: true
+}
+
+);
+// the schema is useless so far
+// we need to create a model using it
+var Things=mongoose.model('Things', thingSchema);
+// make this available to our Node applications
+module.exports=Things;
