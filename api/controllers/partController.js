ca3b80c1d490bc9c7e06f2d95eb94abfb4bbf847
Joseph Brown <joe@sendlove.io>
2016-11-18 14:37:10 -0700

    api returns likes by person

--- api/controllers/partController.js
+++ api/controllers/partController.js
@@ -38,28 +38,57 @@ function renderOne (req, res, id, fieldlist) {
   }
 }
 
+function renderArray (req, res, queryParams, fieldList) {
+  if (!true) {
+      res.status(404)
+        .json("Oh noes! That doesn't appear to be a valid search.");
+  }
+  else {
+    // console.log(JSON.stringify(queryParams));
+    Parts.find(
+      queryParams // build a query object e.g. {colName: {$in: arrayNames}}
+      , fieldList 
+      , function(err, obj) {
+        if (err) throw err;
+        if (obj) {
+          // console.log(obj);
+          res.json(obj);
+        }
+        else {
+          res.status(404)
+            .json("Crikey! I can't find a thing.")
+        }
+      } 
+    ).sort( { _id: 1 } );
+  }
+}
+
+/*
+  get a part by id
+*/
 function getPart(req, res) {
   var id = req.swagger.params.id.value; //req.swagger contains the path parameters
   renderOne(req, res, id, "'id name description thingId personId latitude longitude image category altId partType nValue sValue'");
 }
 
+/*
+  get an array of parts, by thingId
+*/
 function getPartsArray(req, res) {
-  var thingId = req.swagger.params.thingId.value;
-  if (!mongoose.Types.ObjectId.isValid(thingId)) throw Error("Oh noes! That doesn't appear to be a valid id.");
-  Parts.find(
-    {
-      $or:
-      [
-        {'thingId':thingId}
-      ]    
-    }
-    , 'id name description thingId personId latitude longitude image category altId partType nValue sValue'
-    , function(err, obj) {
-      if (err) throw err;
-      console.log(obj);
-      res.json(obj);
-    }
-  );
+  var queryString = {};
+  if (req.swagger.params.thingId != undefined) { // TODO loop through each key and value in the querystring.
+    queryString['thingId'] = req.swagger.params.thingId.value; 
+  }
+  if (req.swagger.params.personId != undefined) { // TODO loop through each key and value in the querystring.
+    queryString['personId'] = req.swagger.params.personId.value; 
+  }
+  if (req.swagger.params.partType != undefined) { // TODO loop through each key and value in the querystring.
+    queryString['partType'] = req.swagger.params.partType.value; 
+  }
+
+  console.log(queryString['thingId']);    
+  console.log(queryString['personId']);    
+  renderArray(req, res, queryString, "'id name description thingId personId latitude longitude imagePath category altId partType nValue sValue'");
 }
 
 function addPart (req, res) {
